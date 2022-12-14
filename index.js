61e900c591fce7bd18e39c037877a5558e57cd6a
Ruth Mwangi <rmwangi22020@gmail.com>
2022-12-14 13:49:06 +0300

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+// Import
+const http = require("http");
+
+// Create Server
+const server = http.createServer(function (request, response) {
+  const method = request.method;
+  const path = request.url;
+  const headers = request.headers;
+
+  const data = {
+    method,
+    path,
+    headers,
+  };
+
+  console.log(data);
+
+  response.end("<p>Success</p>");
+});
+
+server.listen(3000, function () {
+  console.log("My server is running");
+});
