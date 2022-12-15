8c46ace7d0b9aa84cdab6500036efd10fe287014
Ruth Mwangi <rmwangi22020@gmail.com>
2022-12-15 10:55:29 +0300

    render added

---
+++ contact.js
@@ -0,0 +1,8 @@
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+var ada = new Contact("Ada", "Lovelace");
+ada.firstName = "Ada";
+
+ada.lastName = "Lovelace";
